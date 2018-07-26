0e530c899121864974928c82979e4d15d4cbfa44
Boka44 <Guitar101!>
2018-07-26 09:55:26 -0700

    add auth

--- controllers/sync.js
+++ controllers/sync.js
@@ -1,10 +1,27 @@
 const request = require('request');
 const n1ql = require('./n1ql');
 const user = require('../models/user');
+const username = process.env.USERNAME2;
+const password = process.env.PASSWORD;
+const auth = new Buffer(username + ':' + password).toString('base64');
+
+const options = {
+	uri: '',
+	method: 'put',
+	headers: {
+		Authorization: 'Basic ' + auth,
+		'Content-Type': 'application/json'
+	},
+	json: true
+}
 
 module.exports = {
 	syncAddUser: function (id, data) {
-		request.put(`http://localhost:4985/db/${id}?new_edits=true`, {json: data}, (err, response, body) => {
+		options.uri = `http://localhost:4984/db/${id}?new_edits=true`;
+		options['body'] = data;
+
+		console.log(options)
+		request(options, (err, response, body) => {
 			if(err) throw err;
 		})
 	},
@@ -14,8 +31,10 @@ module.exports = {
 		.then((result) => {
 			return new Promise((resolve, reject) => {
 				let data = new user.user(result.user, result.id, result.token, fav);
-				data['_rev'] = result._sync.rev;
-				request.put(`http://localhost:4985/db/${id}?new_edits=true`, {json: data}, (err, response, body) => {
+				data['_rev'] = result._sync.rev;				
+				options.uri = `http://localhost:4984/db/${id}?new_edits=true`;
+				options['body'] = data;
+				request.put(options, (err, response, body) => {
 					if(err) reject(err);
 					resolve(data, err)
 				})
@@ -28,7 +47,9 @@ module.exports = {
 		.then((result) => {
 			return new Promise((resolve, reject) => {
 				data['_rev'] = result._sync.rev;
-				request.put(`http://localhost:4985/db/stock?new_edits=true`, {json: data}, (err, response, body) => {
+				options.uri = `http://localhost:4984/db/stock?new_edits=true`;
+				options['body'] = data;
+				request.put(options, (err, response, body) => {
 					if(err) reject(err);
 					resolve(data)
 				})
